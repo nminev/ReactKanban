3d84f70d5907662e7fe8b82c5eae77cfd1b414ea
mariozisov <zisov4eto@gmail.com>
2019-04-21 14:25:46 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <h1>Hi</h1>;
+  }
+}
+
+export default hot(module)(App);
