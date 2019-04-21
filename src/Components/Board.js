53796ccd2988c0f32a33da392281a50d925b5ad6
Nikolay Minev <niki.minev@yahoo.com>
2019-04-21 18:04:22 +0300

    added axample of dynamically adding of lists

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import ListWrapper from './ListWrapper'
 import "./App.css";
 import { hot } from "react-hot-loader";
+import { throws } from "assert";
 
 
 class Board extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            Lists:Array().fill(null),
+        }
+    }
+    AddingFunction(){
+        var tempArray = this.state.Lists.slice();    
+        tempArray.push(<ListWrapper/>);   
+        this.setState({Lists:tempArray})
+     }
     render() {
       return <div className={"board"} style={{ borderColor:'red' }}>
-      <ListWrapper /><ListWrapper /><ListWrapper /><ListWrapper />
+      {this.state.Lists}
+      <button onClick={() => this.AddingFunction()}>add List</button>
       </div>;
     }
   }
--- src/Components/ListWrapper.js
+++ src/Components/ListWrapper.js
@@ -4,9 +4,13 @@ import "./App.css";
 import { hot } from "react-hot-loader";
 
 
+function ListHeader(props){
+    return <div><h4>List Name Here</h4></div>
+}
+
 class ListWrapper extends Component{ 
     render(){
-      return  <div className={"list"} style={{ borderColor:'blue' }}> this is List <NoteWrapper /></div>
+      return  <div className={"list"} style={{ borderColor:'blue' }}> <ListHeader/ > <NoteWrapper /></div>
     }
   }
 
