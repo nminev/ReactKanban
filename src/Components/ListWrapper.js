58c64e137ae615467cc6466d331a1d67c1545fdd
Nikolay Minev <Nikolay Minev@DESKTOP-SO5VQVK>
2019-04-22 22:08:27 +0300

    added popup , which adds new note, first iteration of the code

--- src/Components/ListWrapper.js
+++ src/Components/ListWrapper.js
@@ -4,14 +4,68 @@ import "./App.css";
 import { hot } from "react-hot-loader";
 
 
-function ListHeader(props){
-    return <div><h4>List Name Here</h4></div>
+
+class ListHeader extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+    <div>
+      <h4>List Header</h4>
+      <button onClick={() => this.props.ChangeShowPopup()}> press me </button>
+    </div>);
+  }
 }
 
 class ListWrapper extends Component{ 
-    render(){
-      return  <div className={"list"} style={{ borderColor:'blue' }}> <ListHeader/ > <NoteWrapper /></div>
+  constructor(props){
+    super(props);
+    this.state={
+      Notes:Array().fill(null),
+      showPopup:true,
     }
   }
+  AddingNote(){
+    var tempArray = this.state.Notes.slice();    
+    tempArray.push(<NoteWrapper/>);   
+    this.setState({
+      Notes:tempArray,
+      showPopup:!this.state.showPopup
+    })
+ }
+ ChangeShowPopup() 
+ {
+  this.setState({
+    showPopup:!this.state.showPopup
+  })
+ }
+    render(){
+      return  <div className={"list"} style={{ borderColor:'blue' }}>
+      <ListHeader  ChangeShowPopup={() => this.ChangeShowPopup()} /> 
+      {this.state.showPopup ? 
+          <Popup
+            text='Close Me'
+            AddingNote={() => this.AddingNote()}
+          />
+          : null
+        }
+      {this.state.Notes}
+      </div>
+    }
+  }
+
+class Popup extends Component {
+    render() {
+      return (
+        <div className='popup'>
+          <div className='popup_inner'>
+            <h1>{this.props.text}</h1>
+          <button onClick={this.props.AddingNote}>close me</button>
+          </div>
+        </div>
+      );
+    }
+}
 
   export default hot(module)(ListWrapper);
