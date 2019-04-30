542f44f6be928fc2f2c7f255afad96458cb7ebff
nikolay.minev <nikolay.minev@smartit.bg>
2019-04-30 16:00:52 +0300

    added navbar

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,3 +1,5 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import React, { Component } from "react";
 import ListWrapper from './ListWrapper'
 import "./App.css";
@@ -19,6 +21,14 @@ class Board extends Component{
      }
     render() {
       return <div className={"board"}>
+      <Navbar bg="dark" variant="dark">
+         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+         <Nav className="mr-auto">
+           <Nav.Link href="#home">Home</Nav.Link>
+           <Nav.Link href="#features">Features</Nav.Link>
+           <Nav.Link href="#pricing">Pricing</Nav.Link>
+         </Nav>
+       </Navbar>
       {this.state.Lists}
       <button onClick={() => this.AddingFunction()}>add List</button>
       </div>;
