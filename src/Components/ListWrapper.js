43548965efc070c897eda6c1e287524f268a517e
nikolay.minev <nikolay.minev@smartit.bg>
2019-05-02 13:46:41 +0300

    prev commit is broken, forgot some stuff

--- src/Components/ListWrapper.js
+++ src/Components/ListWrapper.js
@@ -1,10 +1,9 @@
 import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
 import Card from "react-bootstrap/Card";
+import Popup from "reactjs-popup";
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-import Popup from "reactjs-popup";
+import AddCardForm from "./AddCardForm"
 
 // import NoteWrapper from "./NoteWrapper.js";
 // import "./App.css";
@@ -48,60 +47,6 @@ class ListWrapper extends Component {
 
         {this.state.Cards}
       </div>
-    );
-  }
-}
-
-class AddCardForm extends Component {
-  constructor(props) {
-    super(props);
-    this.cardTitle = "Empty title";
-    this.cardDescription = "Empty title";
-
-    //function bindings
-    this.handleCardTitle = this.handleCardTitle.bind(this);
-    this.handleCardDescription = this.handleCardDescription .bind(this);
-  }
-  handleCardTitle(event) {
-    this.cardTitle = event.target.value;
-  }
-  handleCardDescription(event) {
-    this.cardDescription = event.target.value;
-  }
-  render() {
-    return (
-        <div>
-            Name:
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text>
-                  Card Title
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                onChange={this.handleCardTitle}
-                placeholder="Card Name"
-              />
-            </InputGroup>
-
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text>
-                  Description
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                onChange={this.handleCardDescription}
-                placeholder="Card Description"
-              />
-            </InputGroup>
-          <Button
-            variant="secondary"
-            onClick={() => this.props.AddingCardFunction(this.cardTitle,this.cardDescription)}
-          >
-            Add Card
-          </Button>
-        </div>
     );
   }
 }
